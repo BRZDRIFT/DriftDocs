0a67b4cbaa8ab4b798d12fea3e125044b267c024
unknown <oparowski90@gmail.com>
2026-06-17 19:30:11 +0800

    stuf

--- sq_lexer.py
+++ sq_lexer.py
@@ -32,7 +32,7 @@ class SquirrelLexer(CppLexer):
             'DynVarType', 'ShapeType', 'VictoryStatus', 'SpecialForce',
             'SpecialPlayer', 'AutoAttackTable', 'AttackTable',
             'DamageExtraTable', 'Expr', 'DynValType ', 'BinaryOp',
-            'UnaryOp'
+            'UnaryOp', 'Random'
     )
 
     def get_tokens_unprocessed(self, text, stack=('root',)):
